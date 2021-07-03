a777ffb52cd65c1900ceba1788e6ecbb7226bf2e
Cuong Nguyen Quoc <s3748840@rmit.edu.vn>
2021-07-03 14:45:06 +0700

    POC for login command using Google OAuth

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,22 +1,86 @@
 import express from "express";
 import Command from "@oclif/command";
 import open from "open";
+import passport from "passport";
+import session from "express-session";
 
 export class MyCommand extends Command {
   static description = "description of this example command";
 
   async run() {
-    await open('https://www.npmjs.com/package/open');
     const app = express();
     const port = 3000;
-    app.get("/", (req:express.Request, res:express.Response) => {
-      res.send("The sedulous hyena ate the antelope!");
-      server.close();
-      return console.log(`server is shutdown`);
-    });
-    let server = app.listen(port, () => {
+
+    var userProfile: any;
+
+    app.use(
+      session({
+        resave: false,
+        saveUninitialized: true,
+        secret: "SECRET",
+      })
+    );
+
+    var server = app.listen(port, () => {
       return console.log(`server is listening on ${port}`);
     });
-    server.setTimeout();
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.get('/success', (req, res) => {
+      res.render('pages/success', {user: userProfile});
+    });
+    app.get('/error', (req, res) => res.send("error logging in"));
+     
+    passport.serializeUser(function(user, cb) {
+      cb(null, user);
+    });
+     
+    passport.deserializeUser(function(obj, cb) {
+      cb(null, obj);
+    });
+
+    var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+    const GOOGLE_CLIENT_ID =
+      "564225158115-mps0cifan4iu33k6con3qrl7cf8e6knr.apps.googleusercontent.com";
+    const GOOGLE_CLIENT_SECRET = "mpOA2NTHj0odUev77SbbBUz8";
+
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: GOOGLE_CLIENT_ID,
+          clientSecret: GOOGLE_CLIENT_SECRET,
+          callbackURL: "http://localhost:3000/auth/google/callback",
+        },
+        function (
+          accessToken: any,
+          refreshToken: any,
+          profile: any,
+          done: any
+        ) {
+          console.log(accessToken);
+          server.close();
+          return done(null, userProfile);
+        }
+      )
+    );
+
+    app.get(
+      "/auth/google",
+      passport.authenticate("google", { scope: ["profile", "email"] })
+    );
+
+    app.get(
+      "/auth/google/callback",
+      passport.authenticate(
+        "google", 
+        { successRedirect: '/',
+        failureRedirect: '/error',
+        failureFlash: true }
+      )
+    );
+
+    await open("http://localhost:3000/auth/google");
   }
 }
